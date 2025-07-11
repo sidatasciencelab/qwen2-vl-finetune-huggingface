e3c0a5d8f30d5efe5eb59befb79ac6047bea414f
William Mattingly <62964060+wjbmattingly@users.noreply.github.com>
2025-07-11 14:07:03 -0400

    adjusted

--- train.sh
+++ train.sh
@@ -3,8 +3,8 @@
 #SBATCH --job-name=qwen25_train_test
 #SBATCH --output=qwen25_train_test.txt
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=5
-#SBATCH --gpus=h200
+#SBATCH --cpus-per-task=2
+#SBATCH --gpus=p100:2
 #SBATCH --partition=gpu
 #SBATCH --time=4:00:00
 
